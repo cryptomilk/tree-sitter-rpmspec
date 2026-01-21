9b8993345d1534827a71f567fae76cd89b3f3f70
Andreas Schneider <asn@cryptomilk.org>
2026-01-21 11:01:32 +0100

    chore: Restructure queries/highlights.scm

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,65 +1,103 @@
-; Highlight macro options in parametric expansions
-(macro_option) @variable.parameter
+; Syntax highlighting queries for tree-sitter-rpmspec
+;
+; Organized following the structure of an RPM spec file:
+; 1. Preamble (tags, dependencies)
+; 2. Scriptlets (%prep, %build, %install, etc.)
+; 3. Files section
+; 4. Macros
+; 5. Conditionals
+; 6. Literals and operators
 
-; Simple macro expansion tokens from external scanner
-(macro_simple_expansion
-  "%" @punctuation.special
-  (simple_macro) @function.macro)
-(macro_simple_expansion
-  "%" @punctuation.special
-  (negated_macro) @function.macro)
-(macro_simple_expansion
-  "%" @punctuation.special
-  (special_macro) @constant)
+; =============================================================================
+; PREAMBLE
+; =============================================================================
 
-; Parametric macro expansion: %name [options] [arguments]
-; The name is a function call, options and arguments are parameters
-(macro_parametric_expansion
-  "%" @punctuation.special
-  name: (simple_macro) @function.call)
+; Tags like Name:, Version:, Release:, etc.
+[
+  (tag)
+  (dependency_tag)
+] @type.definition
 
-(macro_parametric_expansion
-  option: (macro_option) @variable.parameter)
+; Dependency comparison operators (>=, <=, =, etc.)
+(dependency_comparison_operator) @operator
 
-(macro_parametric_expansion
-  argument: (word) @variable.parameter)
+; Dependency tag qualifier (e.g., post in Requires(post):)
+(qualifier) @attribute
 
-(macro_parametric_expansion
-  argument: (integer) @number)
+; Boolean dependency operators
+[
+  "if"
+  "else"
+  "unless"
+  "with"
+  "without"
+] @keyword.operator
 
-(macro_parametric_expansion
-  argument: (quoted_string) @string)
+; Boolean dependency parentheses
+(boolean_dependency
+  "(" @punctuation.bracket
+  ")" @punctuation.bracket)
 
-; Macro expansion rules
-(macro_expansion
-  (builtin) @variable.builtin
-  argument: (_) @variable.parameter)
+; Description and package sections
+(description
+  (section_name) @type.definition)
+(package
+  (section_name) @type.definition)
 
-(macro_expansion
-  (identifier) @function.call
-  argument: [
-    (word) @variable.parameter
-    (concatenation
-      (word) @variable.parameter)
-  ])
+; =============================================================================
+; SCRIPTLETS
+; =============================================================================
 
-; Macro definition and undefinition
-(macro_definition
-  "%" @punctuation.special
-  (builtin) @keyword.directive.define
-  (identifier) @keyword.macro)
-(macro_undefinition
-  (builtin) @keyword.directive.define
-  (identifier) @keyword.macro)
+; Build scriptlets (%prep, %build, %install, %check, %clean)
+(prep_scriptlet
+  (section_name) @function.builtin)
+(generate_buildrequires
+  (section_name) @function.builtin)
+(conf_scriptlet
+  (section_name) @function.builtin)
+(build_scriptlet
+  (section_name) @function.builtin)
+(install_scriptlet
+  (section_name) @function.builtin)
+(check_scriptlet
+  (section_name) @function.builtin)
+(clean_scriptlet
+  (section_name) @function.builtin)
 
-; General punctuation for macros
-(macro_expansion
-  "%{" @punctuation.special
-  "}" @punctuation.special) @none
+; Runtime scriptlets (%pre, %post, %preun, %postun, etc.)
+[
+  "%pre"
+  "%post"
+  "%preun"
+  "%postun"
+  "%pretrans"
+  "%posttrans"
+  "%preuntrans"
+  "%postuntrans"
+  "%verify"
+] @function.builtin
 
-; General identifier and builtin rules (must come after specific rules)
-(special_variable_name) @constant
-(builtin) @variable.builtin
+; Trigger scriptlets
+[
+  "%triggerprein"
+  "%triggerin"
+  "%triggerun"
+  "%triggerpostun"
+] @function.builtin
+
+; File trigger scriptlets
+[
+  "%filetriggerin"
+  "%filetriggerun"
+  "%filetriggerpostun"
+  "%transfiletriggerin"
+  "%transfiletriggerun"
+  "%transfiletriggerpostun"
+] @function.builtin
+
+; -----------------------------------------------------------------------------
+; Prep macros (%setup, %autosetup, %patch, %autopatch)
+; -----------------------------------------------------------------------------
 
 (setup_macro
   argument: [
@@ -86,71 +124,14 @@
     (patch_argument) @number
   ])
 
-[
-  (tag)
-  (dependency_tag)
-] @type.definition
+; =============================================================================
+; FILES SECTION
+; =============================================================================
 
-(integer) @number
-(float) @number.float
-(version) @number.float
-
-(comment) @comment
-;(string) @string
-(quoted_string) @string
-
-(description
-  (section_name) @type.definition)
-(package
-  (section_name) @type.definition)
 (files
   (section_name) @type.definition)
-(changelog
-  (section_name) @type.definition)
 
-(prep_scriptlet
-  (section_name) @function.builtin)
-(generate_buildrequires
-  (section_name) @function.builtin)
-(conf_scriptlet
-  (section_name) @function.builtin)
-(build_scriptlet
-  (section_name) @function.builtin)
-(install_scriptlet
-  (section_name) @function.builtin)
-(check_scriptlet
-  (section_name) @function.builtin)
-(clean_scriptlet
-  (section_name) @function.builtin)
-
-[
-  "%pre"
-  "%post"
-  "%preun"
-  "%postun"
-  "%pretrans"
-  "%posttrans"
-  "%preuntrans"
-  "%postuntrans"
-  "%verify"
-] @function.builtin
-
-[
-  "%triggerprein"
-  "%triggerin"
-  "%triggerun"
-  "%triggerpostun"
-] @function.builtin
-
-[
-  "%filetriggerin"
-  "%filetriggerun"
-  "%filetriggerpostun"
-  "%transfiletriggerin"
-  "%transfiletriggerun"
-  "%transfiletriggerpostun"
-] @function.builtin
-
+; File directives
 [
   "%artifact"
   "%attr"
@@ -164,46 +145,101 @@
   "%readme"
 ] @keyword.type
 
-[
-  "!="
-  "<"
-  "<="
-  "="
-  "=="
-  ">"
-  ">="
-  "and"
-  "&&"
-  "or"
-  "||"
-] @operator
+; =============================================================================
+; CHANGELOG
+; =============================================================================
 
-; Dependency comparison operators
-(dependency_comparison_operator) @operator
+(changelog
+  (section_name) @type.definition)
 
-; Boolean dependency operators
-[
-  "if"
-  "else"
-  "unless"
-  "with"
-  "without"
-] @keyword.operator
+; =============================================================================
+; MACROS
+; =============================================================================
 
-; Boolean dependency parentheses
-(boolean_dependency
-  "(" @punctuation.bracket
-  ")" @punctuation.bracket)
+; -----------------------------------------------------------------------------
+; Macro definitions
+; -----------------------------------------------------------------------------
 
-; Dependency tag qualifier (e.g., post in Requires(post):)
-(qualifier) @attribute
+(macro_definition
+  "%" @punctuation.special
+  (builtin) @keyword.directive.define
+  (identifier) @keyword.macro)
 
-; Macro conditional operators
-[
-  "defined"
-  "undefined"
-] @keyword.operator
+(macro_undefinition
+  (builtin) @keyword.directive.define
+  (identifier) @keyword.macro)
 
+; -----------------------------------------------------------------------------
+; Simple macro expansion (%name, %!name, %*, etc.)
+; -----------------------------------------------------------------------------
+
+(macro_simple_expansion
+  "%" @punctuation.special
+  (simple_macro) @function.macro)
+
+(macro_simple_expansion
+  "%" @punctuation.special
+  (negated_macro) @function.macro)
+
+(macro_simple_expansion
+  "%" @punctuation.special
+  (special_macro) @constant)
+
+; -----------------------------------------------------------------------------
+; Parametric macro expansion (%name [options] [arguments])
+; -----------------------------------------------------------------------------
+
+(macro_parametric_expansion
+  "%" @punctuation.special
+  name: (simple_macro) @function.call)
+
+(macro_parametric_expansion
+  option: (macro_option) @variable.parameter)
+
+(macro_parametric_expansion
+  argument: (word) @variable.parameter)
+
+(macro_parametric_expansion
+  argument: (integer) @number)
+
+(macro_parametric_expansion
+  argument: (quoted_string) @string)
+
+; Macro options in parametric expansions
+(macro_option) @variable.parameter
+
+; -----------------------------------------------------------------------------
+; Brace macro expansion (%{name}, %{name:arg}, etc.)
+; -----------------------------------------------------------------------------
+
+(macro_expansion
+  "%{" @punctuation.special
+  "}" @punctuation.special) @none
+
+(macro_expansion
+  (builtin) @variable.builtin
+  argument: (_) @variable.parameter)
+
+(macro_expansion
+  (identifier) @function.call
+  argument: [
+    (word) @variable.parameter
+    (concatenation
+      (word) @variable.parameter)
+  ])
+
+; -----------------------------------------------------------------------------
+; General macro rules
+; -----------------------------------------------------------------------------
+
+(special_variable_name) @constant
+(builtin) @variable.builtin
+
+; =============================================================================
+; CONDITIONALS
+; =============================================================================
+
+; Conditional directives
 [
   "%if"
   "%ifarch"
@@ -217,5 +253,38 @@
   "%endif"
 ] @keyword.conditional
 
-; Fallback rule for identifiers (commented out due to conflicts with parametric macros)
-; (identifier) @variable
+; Macro conditional operators
+[
+  "defined"
+  "undefined"
+] @keyword.operator
+
+; =============================================================================
+; LITERALS AND OPERATORS
+; =============================================================================
+
+; Numbers
+(integer) @number
+(float) @number.float
+(version) @number.float
+
+; Strings
+(quoted_string) @string
+
+; Comments
+(comment) @comment
+
+; Comparison and logical operators
+[
+  "!="
+  "<"
+  "<="
+  "="
+  "=="
+  ">"
+  ">="
+  "and"
+  "&&"
+  "or"
+  "||"
+] @operator
